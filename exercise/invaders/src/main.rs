3e8ed85138419eeb529d0f1285b986d6f1c4c0fe
alo40 <alejandronieto40@gmail.com>
2023-08-15 21:45:07 +0200

    complete invaders frame rendering

--- exercise/invaders/src/main.rs
+++ exercise/invaders/src/main.rs
@@ -15,6 +15,16 @@ use crossterm::{
 
 use rusty_audio::Audio;
 
+use invaders::{
+    frame::{self, new_frame, Drawable, Frame},
+    // invaders::Invaders,
+    // level::Level,
+    // menu::Menu,
+    // player::Player,
+    render,
+    // score::Score,
+};
+
 fn main() -> Result <(), Box<dyn Error>> {
     let mut audio = Audio::new();
     audio.add("explode", "explode.wav");
@@ -31,8 +41,27 @@ fn main() -> Result <(), Box<dyn Error>> {
     stdout.execute(EnterAlternateScreen)?;
     stdout.execute(Hide)?;
 
+    // Render loop in a separate thread
+    let (render_tx, render_rx) = mpsc::channel();
+    let render_handle = thread::spawn(move || {
+        let mut last_frame = frame::new_frame();
+        let mut stdout = io::stdout();
+        render::render(&mut stdout, &last_frame, &last_frame, true);
+        loop {
+            let curr_frame = match render_rx.recv() {
+                Ok(x) => x,
+                Err(_) => break,
+            };
+            render::render(&mut stdout, &last_frame, &curr_frame, false);
+            last_frame = curr_frame;
+        }
+    });
+
     // Game Loop
     'gameloop: loop {
+        // Per-frame init
+        let curr_frame = new_frame();
+
         // Input
         while event::poll(Duration::default())? {
             if let Event::Key(key_event) = event::read()? {
@@ -45,9 +74,16 @@ fn main() -> Result <(), Box<dyn Error>> {
                 }
             }
         }
+
+        // Draw & render
+        let _ = render_tx.send(curr_frame);
+        thread::sleep(Duration::from_millis(1));
+
     }
 
     // Cleanup
+    drop(render_tx);
+    render_handle.join().unwrap();
     audio.wait();
     stdout.execute(Show)?;
     stdout.execute(LeaveAlternateScreen)?;
